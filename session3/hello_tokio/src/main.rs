a8303218394c62f6daf3413ce27e52b4cd27b3a4
chenrun <17610780919@163.com>
2024-07-18 22:07:14 +0800

    feature: session3 add tokio join! and yield_now

--- session3/hello_tokio/src/main.rs
+++ session3/hello_tokio/src/main.rs
@@ -1,20 +1,62 @@
-use tokio::runtime;
-
-async fn hello() {
+async fn hello() -> u32 {
     println!("Hello Tokio");
+
+    10
 }
 
-// fn main() {
-//     let rt = runtime::Builder::new_multi_thread()
-//         .enable_all()
-//         .worker_threads(4)
-//         .build()
-//         .unwrap();
+async fn hello2() -> u32 {
+    println!("Hello Tokio2");
 
-//     rt.block_on(hello());
-// }
+    100
+}
 
-#[tokio::main]
+async fn ticker() {
+    for i in 0..10 {
+        println!("tick {i}");
+
+        tokio::task::yield_now().await;
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
     hello().await;
+
+    let result = tokio::join!(hello(), hello2());
+
+    println!("{:?}", result);
+
+    let (one, two) = result;
+
+    println!("{}, {}", one, two);
+
+    println!("======================================");
+
+    tokio::spawn(ticker());
+
+    hello().await;
+
+    println!("======================================");
+
+    let _ = tokio::join!(
+        tokio::spawn(hello()),
+        tokio::spawn(ticker()),
+        tokio::spawn(ticker())
+    );
+
+    println!("Finished");
 }
+
+/*
+use tokio::runtime;
+
+fn main() {
+    let rt = runtime::Builder::new_multi_thread()
+        .enable_all()
+        .worker_threads(4)
+        .build()
+        .unwrap();
+
+    rt.block_on(hello());
+}
+*/
